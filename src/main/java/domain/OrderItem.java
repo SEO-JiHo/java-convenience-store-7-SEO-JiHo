33e16af4023c40b3fed5a5c6a4a8b31615b5bb75
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-08 00:12:40 +0900

    feat: 인스턴스 생성 단계에서 검증 추가

--- src/main/java/domain/OrderItem.java
+++ src/main/java/domain/OrderItem.java
@@ -1,19 +1,22 @@
-package order;
+package domain;
 
 public class OrderItem {
     private final String name;
     private final int quantity;
 
-    public OrderItem(String name, int quantity) {
+    public OrderItem(String name, int quantity, Products products) {
+        validateOrderItem(products);
         this.name = name;
         this.quantity = quantity;
     }
 
-    public String getName() {
-        return name;
-    }
+    private void validateOrderItem(Products products) {
+        if (!products.containsProduct(name)) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        }
 
-    public int getQuantity() {
-        return quantity;
+        if (!products.hasSufficientStock(name, quantity)) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        }
     }
 }
