efe3f12e4b69c117071322917b60dec7d58b3488
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-07 17:27:44 +0900

    feat: 프로모션 클래스 정의

--- src/main/java/domain/ProductAndPromotionLoader.java
+++ src/main/java/domain/ProductAndPromotionLoader.java
@@ -1,38 +1,42 @@
-package product;
+package domain;
 
 import validation.Validator;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductLoader {
+    private static final int PRODUCT_FIELD_COUNT = 4;
+    private static final int PROMOTION_FIELD_COUNT = 5;
+
     private static final String SPLIT_DELIMITER = ",";
 
     public List<Product> loadProducts(String filePath) {
-        List<Product> products = new ArrayList<>();
-
-        processFile(filePath, products);
+        processFile(filePath);
 
         return products;
     }
 
-    private void processFile(String filePath, List<Product> products) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));) {
-            String oneLine;
-            while ((oneLine = reader.readLine()) != null) {
-                loadSingleProduct(oneLine, products);
-            }
+    private List<String> processFile(String filePath) {
+        try {
+            return Files.lines(Paths.get(filePath))
+                    .skip(1)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
-            System.out.println("[ERROR] 파일 경로에 오류가 있습니다.");
+            throw new IllegalArgumentException("[ERROR] 파일 경로에 오류가 있습니다.");
         }
     }
 
     private void loadSingleProduct(String oneLine, List<Product> products) {
         String[] field = oneLine.split(SPLIT_DELIMITER);
-        Validator.validateProductFieldCount(field);
+        Validator.validateFieldCount(field);
 
         try {
             Product product = new Product(
