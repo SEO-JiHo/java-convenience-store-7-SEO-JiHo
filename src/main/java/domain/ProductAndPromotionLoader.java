581f60c2f1f4f50895d907b53be47cea448f6241
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-07 17:31:13 +0900

    refactor: 한 클래스 내에 상품과 프로모션 파일 불러오는 로직 통합

--- src/main/java/domain/ProductAndPromotionLoader.java
+++ src/main/java/domain/ProductAndPromotionLoader.java
@@ -2,26 +2,39 @@ package domain;
 
 import validation.Validator;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ProductLoader {
+public class ProductAndPromotionLoader {
     private static final int PRODUCT_FIELD_COUNT = 4;
     private static final int PROMOTION_FIELD_COUNT = 5;
-
     private static final String SPLIT_DELIMITER = ",";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public List<Product> loadProducts(String filePath) {
-        processFile(filePath);
+        List<Product> products = new ArrayList<>();
+
+        for (String line : processFile(filePath)) {
+            loadSingleProduct(line, products);
+        }
 
         return products;
+    }
+
+    public List<Promotion> loadPromotions(String filePath) {
+        List<Promotion> promotions = new ArrayList<>();
+
+        for (String line : processFile(filePath)) {
+            loadSinglePromotion(line, promotions);
+        }
+
+        return promotions;
     }
 
     private List<String> processFile(String filePath) {
@@ -36,13 +49,26 @@ public class ProductLoader {
 
     private void loadSingleProduct(String oneLine, List<Product> products) {
         String[] field = oneLine.split(SPLIT_DELIMITER);
-        Validator.validateFieldCount(field);
+        Validator.validateFieldCount(field, PRODUCT_FIELD_COUNT);
 
         try {
             Product product = new Product(
                     field[0], Integer.parseInt(field[1]), Integer.parseInt(field[2]), field[3]);
             products.add(product);
         } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 필드의 값에 오류가 있습니다: " + oneLine);
+        }
+    }
+
+    private void loadSinglePromotion(String oneLine, List<Promotion> promotions) {
+        String[] field = oneLine.split(SPLIT_DELIMITER);
+        Validator.validateFieldCount(field, PROMOTION_FIELD_COUNT);
+
+        try {
+            Promotion promotion = new Promotion(field[0], Integer.parseInt(field[1]), Integer.parseInt(field[2]),
+                    dateFormat.parse(field[3]), dateFormat.parse(field[4]));
+            promotions.add(promotion);
+        } catch (IllegalArgumentException | ParseException e) {
             System.out.println("[ERROR] 필드의 값에 오류가 있습니다: " + oneLine);
         }
     }
