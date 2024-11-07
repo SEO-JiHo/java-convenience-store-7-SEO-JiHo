20b0ec5d889f7135d9563186aa0f3269efb11f0f
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-07 23:58:04 +0900

    feat: 상품이 포함되어 있는지 확인하는 메서드, 재고가 충분한지 확인하는 메서드

--- src/main/java/domain/Products.java
+++ src/main/java/domain/Products.java
@@ -1,4 +1,4 @@
-package product;
+package domain;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,5 +12,25 @@ public class Products {
 
     public List<Product> getProducts() {
         return Collections.unmodifiableList(products);
+    }
+
+    public boolean containsProduct(String name) {
+        for (Product product : products) {
+            if (product.matchName(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasSufficientStock(String name, int purchaseQuantity) {
+        int totalStockQuantity = 0;
+        for (Product product : products) {
+            if (product.matchName(name)) {
+                totalStockQuantity += product.getQuantity();
+            }
+        }
+
+        return totalStockQuantity - purchaseQuantity >= 0;
     }
 }
