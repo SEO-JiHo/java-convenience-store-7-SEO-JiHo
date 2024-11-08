5098db121b1e83e5cd220478a4ab6988281279bf
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-08 23:14:23 +0900

    refactor: 메서드명 수정 및 중복 검증 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,13 +10,13 @@ import domain.Products;
 import validation.Validator;
 
 public class InputView {
-    public String purchaseOrder() {
+    public String getCustomerOrder() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return inputValue();
     }
 
     public boolean requestAddFreeProduct(String name, int quantity) {
-        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n",
+        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n",
                 name, quantity);
 
         return getYesNoInputRecursive();
@@ -43,7 +43,7 @@ public class InputView {
                 return new Order(orders);
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
-                input = purchaseOrder();
+                input = getCustomerOrder();
             }
         }
     }
@@ -67,10 +67,10 @@ public class InputView {
         throw new IllegalArgumentException();
     }
 
-    private void parseOrderItems(String userInput, List<OrderItem> orders, Products products) {
+    public void parseOrderItems(String userInput, List<OrderItem> order, Products products) {
         String[] parts = userInput.split(",");
         for (String part : parts) {
-            validateAndAddOrderItem(part, orders, products);
+            validateAndAddOrderItem(part, order, products);
         }
     }
 
@@ -83,10 +83,6 @@ public class InputView {
     private OrderItem createOrderItem(String[] nameAndQuantity, Products products) {
         String name = nameAndQuantity[0];
         int quantity = Integer.parseInt(nameAndQuantity[1]);
-
-        if (!products.containsProduct(name)) {
-            throw new IllegalArgumentException();
-        }
 
         return new OrderItem(name, quantity, products);
     }
