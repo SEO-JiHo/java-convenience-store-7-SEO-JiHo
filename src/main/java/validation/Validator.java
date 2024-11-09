6f6263b4f3a42cb32644948f72d1511a425f2da7
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-10 02:10:38 +0900

    fix: 상황에 맞는 에러 출력되도록 수정

--- src/main/java/validation/Validator.java
+++ src/main/java/validation/Validator.java
@@ -8,7 +8,7 @@ public class Validator {
     public static String validateWrappedWithBracket(String inputString) {
         if (inputString.charAt(0) != PRODUCT_PREFIX
                 || inputString.charAt(inputString.length()-1) != PRODUCT_SUFFIX) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
 
         return inputString.substring(1, inputString.length()-1);
@@ -32,7 +32,7 @@ public class Validator {
 
     public static void validateNotEmptyValue(String string) {
         if (string.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
     }
 }
