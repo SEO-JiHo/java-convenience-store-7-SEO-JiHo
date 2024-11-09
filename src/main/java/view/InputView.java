496e9754d4e697558820bdb5e33fcf1820e2f46f
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-11-10 02:13:10 +0900

    refactor: 로직 일부 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,23 +15,25 @@ public class InputView {
         return inputValue();
     }
 
-    public boolean requestAddFreeProduct(String name, int quantity) {
+    public boolean requestAddFreeProduct(String name) {
         System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n",
-                name, quantity);
-
+                name);
         return getYesNoInputRecursive();
     }
 
     public boolean requestPurchaseWithoutPromotion(String name, int quantity) {
         System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n",
                 name, quantity);
-
         return getYesNoInputRecursive();
     }
 
     public boolean requestApplyMembershipDiscount() {
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return getYesNoInputRecursive();
+    }
 
+    public boolean requestAdditionalPurchase() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
         return getYesNoInputRecursive();
     }
 
@@ -42,7 +44,7 @@ public class InputView {
                 parseOrderItems(input, orders, products);
                 return new Order(orders);
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+                System.out.println(e.getMessage());
                 input = getCustomerOrder();
             }
         }
@@ -83,6 +85,10 @@ public class InputView {
     private OrderItem createOrderItem(String[] nameAndQuantity, Products products) {
         String name = nameAndQuantity[0];
         int quantity = Integer.parseInt(nameAndQuantity[1]);
+
+        if (quantity < 0) {
+            throw new IllegalArgumentException();
+        }
 
         return new OrderItem(name, quantity, products);
     }
